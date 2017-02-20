47e25302480a97ef8a7dea69ec0848d06ceb5d80
Alex Timofeev <atimofeev@reactant.ru>
2017-02-20 16:39:20 +0700

    Setup `Webpack` for styles compilation

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,26 @@
-var webpack = require('webpack');
+var webpack = require("webpack");
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 module.exports = {
   entry: {
-    blog: './source/javascripts/blog.js'
+    blog: "./source/javascripts/blog.js"
+  },
+  entry: {
+    styles: "./source/stylesheets/blog.scss"
   },
   output: {
-    path: __dirname + '/.tmp/dist',
-    filename: 'javascripts/[name].js',
+    path: __dirname + "/.tmp/dist",
+    filename: "javascripts/[name].js",
   },
+  module: {
+    rules: [
+      {
+        test: /\.(css|scss)$/,
+        use: ExtractTextPlugin.extract([ "css-loader", "sass-loader" ])
+      }
+    ]
+  },
+  plugins: [
+    new ExtractTextPlugin("stylesheets/blog.css"),
+  ]
 };
