aa3ae4f1932739ad066534368976953d258d0278
Alex Timofeev <atimofeev@reactant.ru>
2017-03-01 18:26:51 +0700

    Be able to calculate related articles based on article categories as well

--- lib/blog_helpers.rb
+++ lib/blog_helpers.rb
@@ -1,6 +1,6 @@
 module BlogHelpers
   def blog_categories
-    @blog_categories ||= Hash[article_categories.sort]
+    @blog_categories ||= Hash[system_categories.sort]
   end
 
   def image_url(article)
@@ -12,26 +12,23 @@ module BlogHelpers
   end
 
   def related_articles(article, limit = 3)
-    blog_tags    = blog.tags
-    article_tags = article.tags
+    article_tags       = article.tags
+    article_categories = article_categories(article)
 
-    # @TODO: Use article categories if none of tags were defined in the system
-
-    if blog_tags.any? && article_tags.any?
+    related = if article_tags.any?
       # Fetch articles with tags similar to the current article tags
-      article_tags.map { |t|
-        blog_tags[t]
-      # Unfileter current article
-      }.flatten.uniq.reject { |a|
-        a == article
-      # Randomize and limit
-      }.shuffle.take(limit)
+       article_tags.map { |t| blog.tags[t] }
+    elsif article_categories.any?
+      # Fetch articles with categories similar to the current article tags
+      blog.articles.select { |a| (article_categories(a) & article_categories(article)).any? }
     end
+
+    filter_related(article, related, limit) if related.any?
   end
 
   private
 
-  def categories(article)
+  def article_categories(article)
     category_array(article.data[:categories])
   end
 
@@ -39,13 +36,22 @@ module BlogHelpers
     (categories || "Uncategorized").split(/,\s*/)
   end
 
-  def article_categories
+  def system_categories
     Hash.new { [] }.tap do |c|
       blog.articles.each do |a|
-        categories(a).each do |ac|
+        article_categories(a).each do |ac|
           c[ac] <<= a
         end
       end
     end
+  end
+
+  def filter_related(article, collection, limit)
+    collection
+      .flatten
+      .uniq
+      .reject { |a| a == article }
+      .shuffle
+      .take(limit)
   end
 end
