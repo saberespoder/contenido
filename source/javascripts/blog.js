54f8f98ce6d8d8d1fb04b7adcb9c65802f24dc61
Alex Timofeev <atimofeev@reactant.ru>
2017-04-10 14:26:32 +0700

    Fetch social buttons script only for inner articles

--- source/javascripts/blog.js
+++ source/javascripts/blog.js
@@ -1,10 +1,14 @@
 (function() {
   var readyState = document.readyState;
   if (readyState === "interactive" || readyState === "complete") {
-    var script  = document.createElement("script")
-    script.type = "text/javascript"
-    script.src  = "//platform-api.sharethis.com/js/sharethis.js#property=58ea34c2b23bb10011d6ce9a&product=inline-share-buttons"
-    document.body.appendChild(script);
+    var social  = document.getElementById("js-social")
+
+    if (social) {
+      var script  = document.createElement("script")
+      script.type = "text/javascript"
+      script.src  = "//platform-api.sharethis.com/js/sharethis.js#property=58ea34c2b23bb10011d6ce9a&product=inline-share-buttons"
+      social.appendChild(script)
+    }
   }
   else setTimeout(arguments.callee, 100);
 })();
