2b776de01b39e503d2aa21f491f3b68b3e766f09
Alex Timofeev <atimofeev@reactant.ru>
2017-04-10 14:20:45 +0700

    Call social buttons script on page DOM load only

--- source/javascripts/blog.js
+++ source/javascripts/blog.js
@@ -1,1 +1,10 @@
-//console.debug("Yalo!")
+(function() {
+  var readyState = document.readyState;
+  if (readyState === "interactive" || readyState === "complete") {
+    var script  = document.createElement("script")
+    script.type = "text/javascript"
+    script.src  = "//platform-api.sharethis.com/js/sharethis.js#property=58ea34c2b23bb10011d6ce9a&product=inline-share-buttons"
+    document.body.appendChild(script);
+  }
+  else setTimeout(arguments.callee, 100);
+})();
