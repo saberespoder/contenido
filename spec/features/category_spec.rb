e1edc6355a56f8e1771ff5ee87b0bd5bb2213ab3
Alex Timofeev <atimofeev@reactant.ru>
2017-04-29 17:01:39 +0700

    Add category spec

--- spec/features/category_spec.rb
+++ spec/features/category_spec.rb
@@ -12,4 +12,28 @@ describe "category", type: :feature do
       expect(page).to have_content /Finanzas/i
     end
   end
+
+  # @TODO: It would be great to share this part with index spec
+
+  it "has a defined number of articles per page" do
+    articles = page.find_all(".articles-item")
+    expect(articles.size).to eq(ENV["ARTICLES_PER_PAGE"].to_i)
+  end
+
+  it "has advert block" do
+    expect(page).to have_selector ".related--platform"
+  end
+
+  it "has pagination" do
+    expect(page).to have_selector ".pager"
+    within ".pager" do
+      expect(page).to have_content /Previous/i
+      click_link("Previous")
+    end
+
+    expect(page).to have_selector ".pager"
+    within ".pager" do
+      expect(page).to have_content /Next/i
+    end
+  end
 end
