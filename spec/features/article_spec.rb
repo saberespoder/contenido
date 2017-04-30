d38448d45e2d0ed686ebd5541f5c4689c04b65f4
Alex Timofeev <atimofeev@reactant.ru>
2017-05-01 01:19:30 +0700

    Get test article by name

--- spec/features/article_spec.rb
+++ spec/features/article_spec.rb
@@ -7,7 +7,7 @@ describe "article", type: :feature do
 
   context "any article" do
     before do
-      visit("/articulos/finanzas/campana-de-momentos-consejo-para-padres-con-hijas-entre-8-y-10-anos")
+      click_link("Campaña de Momentos: Consejo para padres con hijas entre 8 y 10 años")
     end
 
     it "has a title" do
@@ -18,13 +18,6 @@ describe "article", type: :feature do
     it "has related articles block" do
       expect(page).to have_selector(".related")
       expect(page.find(".related")).to have_content("Related articles")
-    end
-
-    it "has links to parent categories" do
-      categories = page.find(".article__categories")
-      expect(categories.find_all("a").length).to eq(2)
-      expect(categories).to have_content("Salud")
-      expect(categories).to have_content("Finanzas")
     end
   end
 end
