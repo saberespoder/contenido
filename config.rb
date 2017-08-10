dd6e9a4413633cdfca64156cb6ba977f0214a789
Alex Timofeev <atimofeev@reactant.ru>
2017-08-10 16:37:04 +0700

    Remove staging widget URLs (#83)

--- config.rb
+++ config.rb
@@ -10,7 +10,6 @@ set :content_subtitle, ENV["SUBTITLE"]
 set :platform_url,     ENV["PLATFORM_URL"]
 set :phone_number,     ENV["PHONE_NUMBER"]
 set :feed_articles,    ENV["ARTICLES_PER_FEED"].to_i
-set :is_staging,       ENV["ENV_NAME"] == 'staging' ? true : false
 set :widget_url,       ENV["WIDGET_URL"]
 
 page "/feed.xml", layout: false
